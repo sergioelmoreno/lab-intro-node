5ab8f5a4bcfd0104e1ab1c55dab058d6b65d0651
Sergio del Campo Puerta <sergio@sergioelmoreno.com>
2024-07-22 13:02:53 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]) throw new Error("OutOfBounds")
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (!this.length) throw new Error("EmptySortedList")
+    return this.items[this.items.length - 1]
+  }
 
-  min() {}
+  min() {
+    if (!this.length) throw new Error("EmptySortedList")
+    return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((num, current) => num + current, 0)
+  }
 
-  avg() {}
+  avg() {
+    if (!this.length) throw new Error("EmptySortedList")
+    return this.sum() / this.length
+  }
 }
 
 module.exports = SortedList;
